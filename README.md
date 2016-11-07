c4e81af549dfc10e8b5b67b75951dddcb5202967
Barry de Graaff <info@barrydegraaff.tk>
2016-11-07 10:53:11 +0100

    add prop2xml

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # prop2xml
 A java command line tool to convert a .properties file to a Zimlet config_template.xml
+
+# usage
+
+    java -jar prop2xml.jar rootelem /tmp/newconfig.properties /tmp/config_template.xml
+    
+# input
+    
+     /tmp/newconfig.properties
+     key=value
+    
+# output
+ 
+    /tmp/config_template.xml
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!--
+    DO NOT MANUALLY CHANGE THIS FILE
+    Please read the documentation.
+    -->
+    <zimletConfig name="rootelem" version="1.0"><global>
+    <property name="key">value</property>
+    </global></zimletConfig>
---
+++ src/tk/barrydegraaff/prop2xml.java
@@ -0,0 +1,45 @@
+package tk.barrydegraaff;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+import java.util.Properties;
+
+public class prop2xml {
+
+    public static void main(String[] args) {
+        if (args.length != 3) {
+            throw new IllegalArgumentException("usage: rootElememtName /path/to/config.properties  /path/to/config_template.xml");
+        }
+        try {
+            Properties props = new Properties();
+            FileInputStream inold = new FileInputStream(args[1]);
+            props.load(inold);
+            inold.close();
+
+            PrintWriter out = new PrintWriter(args[2]);
+            out.println("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+            out.println("<!--");
+            out.println("DO NOT MANUALLY CHANGE THIS FILE");
+            out.println("Please read the documentation.");
+            out.println("-->");
+
+            out.println("<zimletConfig name=\""+args[0]+"\" version=\"1.0\"><global>");
+
+
+            Enumeration e = props.propertyNames();
+            while (e.hasMoreElements()) {
+                String key = (String) e.nextElement();
+                out.println("<property name=\""+key+"\">"+props.getProperty(key)+"</property>");
+            }
+
+            out.println("</global></zimletConfig>");
+            out.close();
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return;
+        }
+    }
+}
