02ddadf787a2efc930c501e9a66d033e8ffde22d
Barry de Graaff <info@barrydegraaff.tk>
2017-11-21 09:51:17 +0100

    Create set-java-path.sh

---
+++ set-java-path.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# We only support java versions bundled with Zimbra
+if [[ -x "/opt/zimbra/common/bin/java" ]]
+then
+   #8.7
+    [[ ":$PATH:" != *":/opt/zimbra/common/bin:"* ]] && PATH="/opt/zimbra/common/bin:${PATH}"
+    export PATH
+elif  [[ -x "/opt/zimbra/java/bin/java" ]]
+then
+    #8.6
+    [[ ":$PATH:" != *":/opt/zimbra/java/bin:"* ]] && PATH="/opt/zimbra/java/bin:${PATH}"
+    export PATH
+else
+    echo "Java is not found in /opt/zimbra"
+    exit 1
+fi
